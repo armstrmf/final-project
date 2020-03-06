74c5cdaee6c0f468333612a55add99c08a0d4c80
armstrmf <martin.armstrong@chicagobooth.edu>
2020-03-06 15:14:45 +0000

    AFTER finishing Course resource

---
+++ app/controllers/courses_controller.rb
@@ -0,0 +1,55 @@
+class CoursesController < ApplicationController
+  def index
+    @courses = Course.all.order({ :created_at => :desc })
+
+    render({ :template => "courses/index.html.erb" })
+  end
+
+  def show
+    the_id = params.fetch("path_id")
+    @course = Course.where({:id => the_id }).at(0)
+
+    render({ :template => "courses/show.html.erb" })
+  end
+
+  def create
+    @course = Course.new
+    @course.course_name = params.fetch("query_course_name")
+    @course.address = params.fetch("query_address")
+    @course.par = params.fetch("query_par")
+    @course.yardage = params.fetch("query_yardage")
+
+    if @course.valid?
+      @course.save
+      redirect_to("/courses", { :notice => "Course created successfully." })
+    else
+      redirect_to("/courses", { :notice => "Course failed to create successfully." })
+    end
+  end
+
+  def update
+    the_id = params.fetch("path_id")
+    @course = Course.where({ :id => the_id }).at(0)
+
+    @course.course_name = params.fetch("query_course_name")
+    @course.address = params.fetch("query_address")
+    @course.par = params.fetch("query_par")
+    @course.yardage = params.fetch("query_yardage")
+
+    if @course.valid?
+      @course.save
+      redirect_to("/courses/#{@course.id}", { :notice => "Course updated successfully."} )
+    else
+      redirect_to("/courses/#{@course.id}", { :alert => "Course failed to update successfully." })
+    end
+  end
+
+  def destroy
+    the_id = params.fetch("path_id")
+    @course = Course.where({ :id => the_id }).at(0)
+
+    @course.destroy
+
+    redirect_to("/courses", { :notice => "Course deleted successfully."} )
+  end
+end
---
+++ app/models/course.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: courses
+#
+#  id          :integer          not null, primary key
+#  address     :string
+#  course_name :string
+#  par         :integer
+#  yardage     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Course < ApplicationRecord
+
+  has_many :rounds, :dependent => :destroy
+  has_many :recommendations, :dependent => :destroy
+
+  has_many :fans, :through => :recommendations, :source => :golfer
+
+  validates :course_name, :presence => true
+  validates :course_name, :uniqueness => true
+  validates :address, :presence => true
+  validates :address, :uniqueness => true
+end
